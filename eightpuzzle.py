66252770f8d45aa960d96f0faab221007cd1e21a
kciam001 <kciam001@ucr.edu>
2017-11-01 16:11:24 -0700

    Finished solving function and manhattan distance heuristic

--- eightpuzzle.py
+++ eightpuzzle.py
@@ -10,14 +10,21 @@ class EightPuzzle:
 
 	def __init__(self):
 		self.state = [[1,2,3],
-			 		  [4,0,6],
-			 		  [7,5,8]]
+					  [4,0,6],
+					  [7,5,8]]
 		self.hn = 0
 		self.depth = 0
 		self.parent = None
 
 	def printState(self):
-		print self.state
+
+		for i in range(len(self.state)):
+			print self.state[i][0], self.state[i][1], self.state[i][2]
+
+		print "\n"
+
+		
+
 
 
 	def copy(self):
@@ -25,6 +32,19 @@ class EightPuzzle:
 		for i in range(3):
 			cpy.state[i]= self.state[i][:]
 		return cpy
+
+	def printSolutionTree(self):
+		if self.parent == None:
+			print "Expanding state"
+			self.printState()
+		else:
+			self.parent.printSolutionTree()
+			print "The best state to expand with a g(n) = " + str(self.depth) +  " and h(n) = " + str(self.hn) + " is... "
+			self.printState()
+
+
+
+
 
 
 	def setPuzzle(self, whichPuzzle):
@@ -125,6 +145,185 @@ class EightPuzzle:
 		self.state[yrow][ycol] = xtemp
 
 
+		#recursive function to generate the solution path
+	def pathToSolution(self, path):
+
+
+		if self.parent == None:
+
+			return path
+		else:
+			
+			path.append(self)
+			return self.parent.pathToSolution(path)
+
+	def solvePuzzle(self, heuristicFunction):
+
+		nodes = [self]
+		closed = []
+		maxDepth = 0
+		maxNodesinQueue = -1
+		totalExpanded = 0
+
+		while len(nodes) > 0:
+
+			
+			front = nodes.pop(0)
+
+			if maxNodesinQueue < len(nodes):
+				maxNodesinQueue = len(nodes)
+
+
+			#check if solved
+			if (front.state == goalState):
+				if len(closed) > 0:
+					return front.pathToSolution([]), maxDepth, maxNodesinQueue
+				else:
+					return [front]
+
+			#if it's not solved, see what moves we can make
+			maxDepth += 1
+			possibleMoves = front.createMoves()
+
+			indexNodes = -1
+			indexClosed = -1
+
+			for currMove in possibleMoves:
+
+				#check if we have seen this move
+				count = 0
+
+				for i in nodes:
+					if currMove == i:
+						indexNodes = count
+					count += 1
+
+				count = 0
+				for i in closed:
+					if currMove == i:
+						indexClosed = count
+					count += 1
+
+
+				#setting h(n)
+
+				hn = heuristicFunction(currMove)
+				#total cost
+				cost = hn + currMove.depth
+
+				#if current move hasnt been seen yet
+				if indexNodes == -1 and indexClosed == -1:
+					currMove.hn = hn
+					nodes.append(currMove)
+
+				#else if node is in the queue
+
+				elif indexNodes > -1:
+					#make a copy
+					copy = nodes[indexNodes]
+
+					#check if this move is better
+					if cost < copy.hn + copy.depth:
+						#if it's better copy over the cost
+						copy.hn =hn
+						copy.parent = currMove.parent
+						copy.depth = currMove.depth
+
+
+				#else if node is has already been checked
+				elif indexClosed > -1:
+					#make a copy
+					copy = closed[indexClosed]
+
+					if cost < copy.hn + copy.depth:
+
+						currMove.hn = hn
+						closed.remove(copy)
+						#put node back on queue
+						nodes.append(currMove)
+
+			#close node
+			closed.append(currMove)
+			#sort queue
+			nodes = sorted(nodes, key = lambda p: p.hn + p.depth)
+
+		#if we finish and theres no goal state found, return failure
+		return [], 0, 0
+
+
+
+
+def manhattanDistance(puzzle):
+
+	targetRow = 0
+	targetCol = 0
+	hn = 0
+
+	for row in range(3):
+		for col in range(3):
+
+			val = puzzle.state[row][col]
+
+	
+
+			if(val == 1):
+				targetRow = 0
+				targetCol = 0
+				if(row != targetRow or col != targetCol):
+					hn += (abs(targetRow - row) + abs(targetCol - col))
+
+			if(val == 2):
+				targetRow = 0
+				targetCol = 1
+				if(row != targetRow or col != targetCol):
+					hn += (abs(targetRow - row) + abs(targetCol - col))
+
+			if(val == 3):
+				targetRow = 0
+				targetCol = 2
+				if(row != targetRow or col != targetCol):
+					hn += (abs(targetRow - row) + abs(targetCol - col))
+
+			if(val == 4):
+				targetRow = 1
+				targetCol = 0
+				if(row != targetRow or col != targetCol):
+					hn += (abs(targetRow - row) + abs(targetCol - col))
+
+			if(val == 5):
+				targetRow = 1
+				targetCol = 1
+				if(row != targetRow or col != targetCol):
+					hn += (abs(targetRow - row) + abs(targetCol - col))
+
+			if(val == 6):
+				targetRow = 1
+				targetCol = 2
+				if(row != targetRow or col != targetCol):
+					hn += (abs(targetRow - row) + abs(targetCol - col))
+
+			if(val == 7):
+				targetRow = 2
+				targetCol = 0
+				if(row != targetRow or col != targetCol):
+					hn += (abs(targetRow - row) + abs(targetCol - col))
+
+			if(val == 8):
+				targetRow = 2
+				targetCol = 1
+				if(row != targetRow or col != targetCol):
+					hn += (abs(targetRow - row) + abs(targetCol - col))
+
+
+	return hn
+
+
+			
+
+
+
+
+
 
 
 
@@ -134,10 +333,29 @@ class EightPuzzle:
 def main():
 	
 	puzzle = EightPuzzle()
-	print "Type \"1\" to use the default puzzle or \"2\" to enter your own: "
+	print "Type \"1\" to use the default puzzle or \"2\" to enter your own: ",
 	whichPuzzle = input()
 
+
 	puzzle.setPuzzle(whichPuzzle)
+
+	print "Original: "
+	puzzle.printState()
+
+	print "\n"
+	solutionPath, numberOfMoves, maxNodes = puzzle.solvePuzzle(manhattanDistance)
+
+	solutionPath.reverse
+
+	solutionPath[0].printSolutionTree()
+
+	#solutionPath[0].printState()
+	#print "\n"
+	#solutionPath[0].parent.printState()
+
+	
+	print "The maximum number of nodes in the queue at any one time was " + str(maxNodes) +"."
+	print "The depth of the goal node was " + str(numberOfMoves) +"."
 
 	#puzzle.printState()
 	
@@ -145,7 +363,7 @@ def main():
 	#b = 0, 1
 
 	#puzzle.swap(a, b)
-	puzzle.createMoves()
+	#puzzle.createMoves()
 
 	#puzzle.printState()
 
